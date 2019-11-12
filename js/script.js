fd2f1805f4c6820307109d2706a463e2aa1e60fb
JeyKevin <yeremiakevin51@gmail.com>
2019-11-12 12:40:55 +0700

    upload ke github

---
+++ js/script.js
@@ -0,0 +1,43 @@
+//$(".page-scroll").click(function(event){
+	//console.log("bisa");
+	//ambil elemen link yang ada tanda #
+// 	if(this.hash!==""){
+
+// 		//matikan link
+// 		//event.preventDefault();
+
+// 		// ambil nilai dari link
+// 		var hash = this.hash;
+
+// 		console.log(hash);
+
+// 		//animasi scroll
+// 		// $("html, body").animate({
+// 		// 	scrollTop: $(hash).offset().top
+// 		// }, 1500, "swing" , function(){
+// 		// 	window.location.hash=hash;
+		
+// 		// });
+
+// // 		//untuk effect section
+// // 		if(hash=="#about") {
+// // 			$(hash).effect("bounce",2000);
+// // 			$("#about-kiri").effect("slide",500);
+// // 		} else if(hash=="#contact") {
+// // 			$(hash).effect("slide",2000);
+// // 		} else if(hash=="#portfolio") {
+// // 			$(hash).effect("shake",2000);
+// // 		}
+
+
+		
+// 	}
+//});
+
+// $(".img-thumbnail").mouseover(function(){
+// 	$(this).effect("shake",1000);
+// });
+
+// $(".page-scroll").click(function(){
+// 	$(this).effect("bounce",1000);
+// });
